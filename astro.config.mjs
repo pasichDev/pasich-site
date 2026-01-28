e9f4ec4906dda3ee7152ff3e9f1b84e1c8b0996c
pasichDev <apasichnik9@gmail.com>
2026-01-28 17:10:52 +0200

    update ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
     site: 'https://pasichdev.xyz',
+    integrations: [sitemap()],
     build: {
         assets: '_assets'
     }
