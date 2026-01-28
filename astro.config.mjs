6870bb8068c1edf2062dab4907ef242c613f33ef
pasichDev <apasichnik9@gmail.com>
2026-01-28 17:43:56 +0200

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-    site: 'https://pasichdev.xyz',
+    site: 'https://pasichdev.xyz/',
     integrations: [sitemap()],
     build: {
         assets: '_assets'
